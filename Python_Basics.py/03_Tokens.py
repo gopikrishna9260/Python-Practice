36fc182bed27de3b0f558211d5f843eaa97249ba
gopikrishna9260 <malladhigopi2@gmail.com>
2026-08-07 18:03:28 +0530

    Updated Tokens Practice

--- Python_Basics.py/03_Tokens.py
+++ Python_Basics.py/03_Tokens.py
@@ -15,5 +15,61 @@ a = 10
 b = 20
 print(a + b)
 # Tokens: a, =, 10, b, =, 20, print, (, a, +, b, )
+# Question:3
+# Assign these values:
+# language = Python
+# version = 3.13
+# Print both variables.
+# Identify:
+# 1. Keywords
+# 2. Identifiers
+# 3. Literals
+# 4. Operators
+# 5. Separators
+language = "python"
+version = 3.13
+print(language, version) 
+# Keywords: print
+# Identifiers: language, version
+# Literals: "python", 3. 13
+# Operators: =, ,
+# Separators: (, ), ,
+# Question:4
+# Create the following variables:
+# student_name = Gopi Krishna
+# roll_number = 101
+# cgpa = 8.5
+# Print all the variables.
+# Count the total number of tokens.
+student_name = "Gopi krishna"
+roll_number = 101
+cgpa = 8.5
+print(student_name, roll_number, cgpa)
+# Total number of tokens: 15
+# Question:5
+# Write a Python program that contains:
+# 1. One keyword
+# 2. Two identifiers
+# 3. Two literals
+# 4. One operator
+# 5. Two separators
+# Identify each token.
+name = "Gopi krishna"
+age = 25
+print(name, age)
+# Keywords: print
+# Identifiers: name, age
+# Literals: "Gopi krishna", 25
+# Operators: =
+# Separators: (, ), , 
+
+
+
+
+
+
+
+
+
 
 
