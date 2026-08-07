5e566c321c859f0b9dac910c35f98f9cbda8fb60
gopikrishna9260 <malladhigopi2@gmail.com>
2026-08-07 13:56:46 +0530

    Tokens topic completed

---
+++ Python_Basics.py/03_Tokens.py
@@ -0,0 +1,19 @@
+# Question:1
+# Create a variable named 'name' and assign your name.
+# Print the variable.
+# Identify all the tokens in the program.
+name = "Gopi krishna"
+print(name)
+# Tokens: name, =, "Gopi krishna", print, (, name, )
+# Question:2
+# Create two variables:
+# a = 10
+# b = 20
+# Print the sum of a and b.
+# Identify all the tokens in the program.
+a = 10
+b = 20
+print(a + b)
+# Tokens: a, =, 10, b, =, 20, print, (, a, +, b, )
+
+
