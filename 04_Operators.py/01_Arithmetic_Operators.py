7036b968c5d55ba752239290f5759a75bb7d5fd5
gopikrishna9260 <malladhigopi2@gmail.com>
2026-08-07 19:28:12 +0530

    Added Operators folder structure

---
+++ 04_Operators.py/01_Arithmetic_Operators.py
@@ -0,0 +1,108 @@
+#Question1
+# Create two variables:
+# a = 20
+# b = 10
+# Perform the following operations:
+# Addition (+)
+# Subtraction (-)
+# Multiplication (*)
+# Division (/)
+# Print all the results.
+a = 20
+b = 10
+addition =a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+# Question2
+# Create two variables:
+# x = 25
+# y = 4
+# Perform:
+# Floor Division (//)
+# Modulus (%)
+# Exponent (**)
+# Print all the results.
+x = 25
+y = 4
+floor_division = x // y
+modulus = x ** y
+print("floor_division:", floor_division)
+print("modulus:",modulus)
+# Question3
+# Create two variables:
+# num1 = 15
+# num2 = 5
+# Perform all Arithmetic Operators:
+# +, -, *, /, //, %, **
+# Print each result.
+num1 = 15
+num2 = 5
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+divistion = num1 /num2
+floor_divistion = num1 // num2 
+modulus = num1 % num2
+exponent = num1 ** num2
+print("addition:", addition)
+print("subtraction:", subtraction)
+print("multiplication:", multiplication)
+print("divistion:", divistion)
+print("floor_divistion:", floor_divistion)
+print("modulus", modulus)
+print("exponent", exponent)
+# Question4
+# Create two variables:
+# length = 12
+# width = 6
+# Find:
+# Area using Multiplication
+# Perimeter using Addition
+# Print the results.
+length = 12
+width = 6
+area = length * width
+perimeter = 2 * (length + width)
+print("Area:", area)
+print("perimeter:", perimeter)
+# Question5
+# Create two variables:
+# a = 9
+# b = 2
+# Perform:
+# Addition
+# Subtraction
+# Multiplication
+# Division
+# Floor Division
+# Modulus
+# Exponent
+# Print all the results.
+a = 9
+b = 2
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+divistion = a / b
+floor_divistion = a // b
+modulus = a % b
+exponent = a ** b
+print("addition:", addition)
+print("subtraction:",subtraction)
+print("multiplication:", multiplication)
+print("divistion:", divistion)
+print("floor_divistion:", floor_divistion)
+print("modulus:", modulus)
+print("exponent:", exponent)
+
+
+
+
+
+
